ce9006bf8499e91ca5fb9c8cf7f8e1caa0914f8e
Adam Kinzer <adamkinzer915@gmail.com>
2016-01-22 13:53:30 -0700

    Added a method

--- src/pixLab/classes/Picture.java
+++ src/pixLab/classes/Picture.java
@@ -99,6 +99,33 @@ public class Picture extends SimplePicture
     }
   }
   
+  public void zeroRed()
+  {
+	  Pixel[][] redPixels = this.getPixels2D();
+	  for(int row = 0; row < redPixels.length; row++)
+	  {
+		  for(int col = 0; col < redPixels[0].length; col++)
+		  {
+			  Pixel currentPixel = redPixels[row][col];
+			  currentPixel.setRed(0);
+			  
+			  //redPixels[row][col].setRed(0);
+		  }
+	  }
+  }
+  
+  public void zeroGreen()
+  {
+	  Pixel[][] greenPixels = this.getPixels2D();
+	  for(int row = 0; row < greenPixels.length; row++)
+	  {
+		  for(int col = 0; col < greenPixels[0].length; col++)
+		  {
+			  greenPixels[row][col].setGreen(0);
+		  }
+	  }
+  }
+  
   /** Method that mirrors the picture around a 
     * vertical mirror in the center of the picture
     * from left to right */
@@ -224,9 +251,11 @@ public class Picture extends SimplePicture
    */
   public static void main(String[] args) 
   {
-    Picture beach = new Picture("beach.jpg");
+    Picture beach = new Picture("butterfly1.jpg");
     beach.explore();
+    beach.zeroRed();
     beach.zeroBlue();
+    beach.zeroGreen();
     beach.explore();
   }
   
